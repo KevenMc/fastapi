c144c155fa9e861fa560aa9c76e636853af1e951
KevenMc <djeloniticsPASSWORD123>
2023-01-30 17:45:43 +0000

    update post return model

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends
-from typing import Optional
+from typing import Optional, List
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
@@ -31,7 +31,6 @@ while True:
         time.sleep(2)
 
 
-
 # HOME PAGE
 @app.get('/')
 async def root():
@@ -39,16 +38,16 @@ async def root():
 
 
 # GET ALL POSTS
-@app.get('/posts')
+@app.get('/posts', response_model = List[schemas.PostResponse])
 async def get_posts(db: Session = Depends(get_db)):
     # cursor.execute("""SELECT * FROM posts""")
     # posts = cursor.fetchall()
     posts = db.query(models.Post).all()
-    return {'data': posts}
+    return posts
 
 
 # CREATE POST
-@app.post('/posts', status_code=status.HTTP_201_CREATED)
+@app.post('/posts', status_code=status.HTTP_201_CREATED, response_model = schemas.PostResponse)
 async def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
     #                    (post.title,
@@ -56,31 +55,31 @@ async def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
     #                     post.published))
     # new_post = cursor.fetchone()
     # conn.commit()
-    new_post = models.Post(title=post.title,
-                           content=post.content,
-                           published=post.published)
+    new_post = models.Post(title = post.title,
+                           content = post.content,
+                           published = post.published)
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-    return {'data': new_post}
+    return new_post
 
 
 # READ POST BY ID
-@app.get('/posts/{id}')
+@app.get('/posts/{id}', response_model = schemas.PostResponse)
 def get_post(id: int, response: Response, db: Session = Depends(get_db)):
     # cursor.execute("""SELECT * FROM posts WHERE id = %s""",
     #                (str(id)))
     # post = cursor.fetchone()
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if post == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
                             detail="This post does not exist, perhaps it has been deleted")
-    return {'data': post}
+    return post
 
 
 # UPDATE POST BY ID
-@app.patch('/posts/{id}')
-async def create_post(id: int, update_post: schemas.PostCreates, db: Session = Depends(get_db)):
+@app.patch('/posts/{id}', response_model = schemas.PostResponse)
+async def create_post(id: int, update_post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
     #                    (post.title,
     #                     post.content,
@@ -94,15 +93,15 @@ async def create_post(id: int, update_post: schemas.PostCreates, db: Session = D
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post = post_query.first()
     if post == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
                             detail="This post does not exist, perhaps it has been deleted")
     post_query.update(update_post.dict(), synchronize_session=False)
     db.commit()
-    return {'data': post_query.first()}
+    return post_query.first()
 
 
 # DELETE POST
-@app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
+@app.delete('/posts/{id}', status_code = status.HTTP_204_NO_CONTENT)
 async def delete_post(id: int, db: Session = Depends(get_db)):
     # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""",
     #                (str(id)))
--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,5 @@
-
 from pydantic import BaseModel
-
+from datetime import datetime
 
 class PostBase(BaseModel):
     title: str
@@ -10,3 +9,11 @@ class PostBase(BaseModel):
 
 class PostCreate(PostBase):
     pass
+
+
+class PostResponse(PostBase):
+    id: int
+    created_at: datetime
+    
+    class Config:
+        orm_mode=True
